da031c81a18df99443ba6b39c987f963d9694b25
nickzdeb <nickzdeb@gmail.com>
2016-11-22 16:50:18 -0700

    Updated the gameConditionTests

--- src/GravityGolfGame/Board.java
+++ src/GravityGolfGame/Board.java
@@ -1,5 +1,22 @@
 package GravityGolfGame;
 
 public class Board {
+	
+	private static Board theInstance = new Board();
+	
+	private Board() {
+		
+	}
+	
+	public static Board getInstance() {
+		return theInstance;
+	}
 
+	public static boolean isGameOver() {
+		//I will create this method when more pars are implemented (Nick)
+		//Player will win when the ball reaches a certain spot, simple condition statement
+		//TO DO: Need to imlplement the ball
+		
+		return false;
+	}
 }
