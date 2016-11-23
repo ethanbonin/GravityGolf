b9ac80fa7d955509038545a471d4004e1b38953b
nickzdeb <nickzdeb@gmail.com>
2016-11-22 20:29:59 -0700

    Completed the gameConditions and LevelLoad tests, started to initialize the board, may ned to modfy csv file for blank cells inside borders

--- src/GravityGolfGame/BoardCell.java
+++ src/GravityGolfGame/BoardCell.java
@@ -2,11 +2,21 @@ package GravityGolfGame;
 
 public class BoardCell {
 	
+	char boardCellInitial;
+	
 	public BoardCell(){
 		
 	}
 	
 	public BoardCell(int x, int y){
+		
+	}
+
+	public char getInitial() {
+		return boardCellInitial;
+	}
+	
+	public void setInitial() {
 		
 	}
 	
--- src/test/LevelLoadTest.java
+++ src/test/LevelLoadTest.java
@@ -1,5 +1,26 @@
 package test;
 
-public class LevelLoadTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import GravityGolfGame.Ball;
+import GravityGolfGame.Board;
+
+public class LevelLoadTest {
+	
+	@BeforeClass
+	public static void setup(){
+		Board board = Board.getInstance();
+		board.setConfigFiles("GravityGolfBoard.csv");
+	}
+	
+	@Test
+	public void testBoardLoad() {
+		assertEquals(true, Board.getCellAt(24, 25).getInitial() == 'W');
+		assertEquals(true, Board.getCellAt(24, 0).getInitial() == 'E');
+		assertEquals(true, Board.getCellAt(0, 4).getInitial() == 'N');
+		assertEquals(true, Board.getCellAt(24, 1).getInitial() == 'S');
+	}
 }
