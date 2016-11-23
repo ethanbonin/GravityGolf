33b984a0b27f6760b1ac8eb2f9ea825cc00874e0
nickzdeb <nickzdeb@gmail.com>
2016-11-22 18:15:53 -0700

    updated game conditions

--- src/test/GameConditionTest.java
+++ src/test/GameConditionTest.java
@@ -1,5 +1,29 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import GravityGolfGame.Board;
+
 public class GameConditionTest {
 
+	
+	
+		@BeforeClass
+		public void setup(){
+			Board board = Board.getInstance();
+		}
+		
+		@Test
+		public void testPause(){
+			
+		}
+		
+		@Test
+		public void testWin(){
+			assertEquals(true, Board.isGameOver());
+		}
+		
 }
