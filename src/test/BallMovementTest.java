3ef1e71b96fe0c5ceeb13ac0b718756ad18effa3
ethanbonin <ethanbonin@gmail.com>
2016-11-22 20:12:16 -0700

    Added some functions and private variables

--- src/test/BallMovementTest.java
+++ src/test/BallMovementTest.java
@@ -1,11 +1,21 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import GravityGolfGame.BoardCell;
+
 public class BallMovementTest {
 	
 	
+	
+	
 	@Test
-	public void someTest() {
+	public void BoardCellContains() {
+		BoardCell circle = new BoardCell(20,30);
 		
+		assertEquals(false, true);
 	}
 	
 
