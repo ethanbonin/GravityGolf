1e5fb071de25f970e89e5b45286d046e8a3cb35e
Zachary Barnum <zbarnum@mines.edu>
2016-12-01 10:16:51 -0700

    Implemented simple ball bouncing that finds a new vector and velocity for the ball after interaction without any exterior contraints like gravity or friction

--- src/GravityGolfGame/Ball.java
+++ src/GravityGolfGame/Ball.java
@@ -4,21 +4,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Ball {
-
+	
+	protected int x;
+	protected int y;
+	protected Vector velocity;
+	private double radius;
+	boolean isMoving;
+	private Color color;
+	
 	public Ball() {
 		x = 0;
 		y = 0;
+		this.isMoving = false;
 		velocity = new Vector();
 	}
-	
-	int x;
-	int y;
-	Vector velocity;
-	double radius;
-	
-	boolean isMoving;
-	
-	private Color color;
 	
 	public void move(){
 		x += velocity.getX();
